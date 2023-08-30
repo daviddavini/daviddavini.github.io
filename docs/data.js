2ce3060eace71788f29305246cb1c1b35a62f67d
David Davini <dndavini3.14@gmail.com>
2023-08-29 21:17:16 -0700

    make data.js, use for caffeine plot

---
+++ docs/data.js
@@ -0,0 +1,243 @@
+let data = [
+  { 
+    day: '8/19',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+    ],
+  },
+  { 
+    day: '8/20',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'green tea',
+        name: 'Bigelow Green Tea',
+        serving: '2 steeps',
+        amount: 45,
+      },
+      {
+        type: 'green tea',
+        name: 'Bigelow Green Tea',
+        serving: '2 steeps',
+        amount: 45,
+      },
+    ],
+  },
+  { 
+    day: '8/21',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+    ],
+  },
+  { 
+    day: '8/22',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+    ],
+  },
+  { 
+    day: '8/23',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+      {
+        type: 'green tea',
+        name: 'Cold Brew Green Tea Bags',
+        serving: '2 steeps',
+        amount: 75,
+      },
+    ],
+  },
+  { 
+    day: '8/24',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+      {
+        type: 'green tea',
+        name: 'Cold Brew Green Tea Bags',
+        serving: '2 steeps',
+        amount: 75,
+      },
+    ],
+  },
+  { 
+    day: '8/25',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Tropical Vibe',
+        amount: 200,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+    ],
+  },
+  { 
+    day: '8/26',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+    ],
+    reading: [
+      {
+        type: 'textbook',
+        name: 'Mastering Regular Expressions',
+        start: 1,
+        end: 68,
+        rating: 5,
+      },
+      {
+        type: 'nonfiction',
+        name: 'The Autobiography of Malcolm X',
+        start: 171,
+        end: 183,
+        rating: 3,
+      },
+    ],
+  },
+  { 
+    day: '8/27',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2 tsp',
+        amount: 120,
+      },
+      {
+        type: 'green tea',
+        name: 'Bigelow Green Tea',
+        serving: '3 steeps',
+        amount: 45,
+      },
+    ],
+    reading: [
+      {
+        type: 'textbook',
+        name: 'Mastering Regular Expressions',
+        start: 68,
+        end: 92,
+        rating: 4,
+      },
+      {
+        type: 'nonfiction',
+        name: 'The Autobiography of Malcolm X',
+        start: 183,
+        end: 218,
+        rating: 4,
+      },
+    ],
+  },
+  { 
+    day: '8/28',
+    caffeine: [
+      {
+        type: 'coffee',
+        name: 'Oro Instant Coffee',
+        serving: '2.5 tsp',
+        amount: 150,
+      },
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+    ],
+    reading: [
+      {
+        type: 'textbook',
+        name: 'Mastering Regular Expressions',
+        start: 92,
+        end: 95,
+        rating: 3,
+      },
+    ],
+  },
+  { 
+    day: '8/29',
+    caffeine: [
+      {
+        type: 'energy drink',
+        name: 'Celsius Peach Vibe',
+        amount: 200,
+      },
+      {
+        type: 'black tea',
+        name: 'Twinnings Chai Tea',
+        serving: '3 steeps',
+        amount: 75,
+      },
+    ],
+    reading: [
+      {
+        type: 'nonfiction',
+        name: 'Inventive Wizard: George Westinghouse',
+        start: 11,
+        end: 54,
+        rating: 5,
+      },
+    ],
+  },
+];
